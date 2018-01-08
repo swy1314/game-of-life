e76107ad061378986813e834cc22469f9b3aaf7e
swy1314 <xjj314@163.com>
2018-01-08 16:17:44 +0800

    test2

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenSpawningANewGeneration.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenSpawningANewGeneration.java
@@ -27,7 +27,7 @@ public class WhenSpawningANewGeneration {
     public void theIndexUrlShouldDisplayTheHomePage() {
         HomePageController controller = new HomePageController();
         ModelAndView homeView = controller.index();
-        assertThat(homeView.getViewName(), is("home1"));
+        assertThat(homeView.getViewName(), is("home"));
     }
 }	
 
